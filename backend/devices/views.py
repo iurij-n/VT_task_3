c3cc6bd805663de416f6697784a816b9248300dc
iurij-n <iurij.novickov2016@yandex.ru>
2023-01-04 09:27:58 +0300

    some fix

--- backend/devices/views.py
+++ backend/devices/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets 
+from rest_framework import viewsets
 
 from .models import Device
 from .serializers import DeviceSerializer
@@ -7,4 +7,3 @@ from .serializers import DeviceSerializer
 class DeviceViewSet(viewsets.ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
- 
